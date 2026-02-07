a5d938df8364a2253ebdbd482ef094aab861c8a7
elbriga <gabrielortizlour@gmail.com>
2026-02-07 15:26:43 -0300

    instalador

---
+++ scripts/install.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+echo "Conferindo usuario btc"
+sudo useradd -r -s /usr/sbin/nologin btc &> /dev/null
+sudo chown -R btc:btc /opt/btc-tracker/
+
+echo "Instalando"
+cp /opt/btc-tracker/config/btc-tracker.service /etc/systemd/system/
+sudo systemctl daemon-reexec
+sudo systemctl daemon-reload
+
+echo "Habilitando btc-tracker no boot"
+sudo systemctl enable btc-tracker
+
+echo "Iniciando"
+sudo systemctl start btc-tracker
+
